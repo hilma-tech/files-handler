071700e20a99a5b1afaf408fb8ad7a377f5d408a
roniasah101 <ron@optunity.co.il>
2020-01-22 12:05:20 +0200

    update disabled mode at preview widget

--- client/components/PreviewWidget.js
+++ client/components/PreviewWidget.js
@@ -16,12 +16,12 @@ export default class PreviewWidget extends Component {
                 {this.props.showPopup && <div className="dark-background" onClick={this.props.toggleShowPopup} />}
                 <div className={`image-popup ${this.props.showPopup ? "scale-in-center" : "scale-out-center"}`} >
                     {this.props.chosenImg}
-                    {(this.props.enableEdit || this.props.enableDelete) &&
+                    {(this.props.enableEdit || this.props.enableDelete) && !this.props.disabled &&
                         <div>
                             <div className="tool-bar-dark-background" />
                             <div className="tool-bar">
-                                {this.props.enableEdit && !this.props.disabled && <label className="tool-bar-label" htmlFor={this.props.inputId}><img className="edit" src={require('../../imgs/edit.svg')} /></label>}
-                                {this.props.enableDelete && !this.props.disabled && <img className="bin" src={require('../../imgs/bin.svg')} onClick={this.props.removeFile} />}
+                                {this.props.enableEdit && <label className="tool-bar-label" htmlFor={this.props.inputId}><img className="edit" src={require('../../imgs/edit.svg')} /></label>}
+                                {this.props.enableDelete && <img className="bin" src={require('../../imgs/bin.svg')} onClick={this.props.removeFile} />}
                             </div>
                         </div>}
                 </div>
