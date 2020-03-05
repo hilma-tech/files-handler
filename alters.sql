f5a1571113c934d2e4a6f5bd481a39577de3cea1
roniasah101 <ron@optunity.co.il>
2020-03-05 10:22:09 +0200

    update alters

--- alters.sql
+++ alters.sql
@@ -9,31 +9,35 @@ CREATE TABLE `games_images` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `Audio` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) DEFAULT NULL,
-  `description` text,
-  `created` datetime DEFAULT NULL,
-  `format` enum('docx','doc','pdf','mp3','wav') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `category` varchar(100) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `owner` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
-
-CREATE TABLE `Video` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) DEFAULT NULL,
-  `description` text,
-  `created` datetime DEFAULT NULL,
-  `format` enum('docx','doc','pdf','mp3','wav') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `category` varchar(100) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `owner` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
-
 ALTER TABLE Images ADD width int;
+
+-- CREATE TABLE `Audio` (
+--   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+--   `title` varchar(200) DEFAULT NULL,
+--   `description` text,
+--   `created` datetime DEFAULT NULL,
+--   `format` enum('docx','doc','pdf','mp3','wav') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+--   `category` varchar(100) DEFAULT NULL,
+--   `modified` datetime DEFAULT NULL,
+--   `owner` int(11) DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
+
+-- CREATE TABLE `Video` (
+--   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+--   `title` varchar(200) DEFAULT NULL,
+--   `description` text,
+--   `created` datetime DEFAULT NULL,
+--   `format` enum('docx','doc','pdf','mp3','wav') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+--   `category` varchar(100) DEFAULT NULL,
+--   `modified` datetime DEFAULT NULL,
+--   `owner` int(11) DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
+
+
+-- *******************DON'T DO!!!!*****************
+--__________________________________________________
 
 --ALTER TABLE Images DROP COLUMN width;
 
