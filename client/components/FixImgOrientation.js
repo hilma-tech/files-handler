03fa770a985805454155b62e7b53e53133b18466
reut avital <reutavital@carmel6000.amitnet.org>
2020-06-03 17:45:30 +0300

    fix problem

--- client/components/FixImgOrientation.js
+++ client/components/FixImgOrientation.js
@@ -31,27 +31,31 @@ const FixImgOrientation = {
             var length = view.byteLength;
             var offset = 2;
             while (offset < length) {
-                var marker = view.getUint16(offset, false);
-                offset += 2;
-                if (marker === 0xFFE1) {
-                    if (view.getUint32(offset += 2, false) != 0x45786966) {
-                        return cb(-1);
-                    }
-                    var little = view.getUint16(offset += 6, false) === 0x4949;
-                    offset += view.getUint32(offset + 4, little);
-                    var tags = view.getUint16(offset, little);
+                try {
+                    var marker = view.getUint16(offset, false);
                     offset += 2;
-                    for (var i = 0; i < tags; i++) {
-                        if (view.getUint16(offset + (i * 12), little) == 0x0112) {
-                            return cb(view.getUint16(offset + (i * 12) + 8, little));
+                    if (marker === 0xFFE1) {
+                        if (view.getUint32(offset += 2, false) != 0x45786966) {
+                            return cb(-1);
+                        }
+                        var little = view.getUint16(offset += 6, false) === 0x4949;
+                        offset += view.getUint32(offset + 4, little);
+                        var tags = view.getUint16(offset, little);
+                        offset += 2;
+                        for (var i = 0; i < tags; i++) {
+                            if (view.getUint16(offset + (i * 12), little) == 0x0112) {
+                                return cb(view.getUint16(offset + (i * 12) + 8, little));
+                            }
                         }
                     }
-                }
-                else if ((marker & 0xFF00) !== 0xFF00) {
-                    break;
-                }
-                else {
-                    offset += view.getUint16(offset, false);
+                    else if ((marker & 0xFF00) !== 0xFF00) {
+                        break;
+                    }
+                    else {
+                        offset += view.getUint16(offset, false);
+                    }
+                } catch (err) {
+                    return cb(-1);
                 }
             }
             return cb(-1);
